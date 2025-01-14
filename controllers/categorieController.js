cf13b020b91fa19db3c6dd7b696a74c19f67c7c1
Fahad Hasan <44fahadhasan@gmail.com>
2025-01-14 19:21:08 +0600

    some upd in categorey

--- controllers/categorieController.js
+++ controllers/categorieController.js
@@ -97,7 +97,14 @@ exports.getAllCategoriesList = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Categories list fetch successfully",
-      data: modified_categories_list,
+      data: [
+        ...modified_categories_list,
+        {
+          label: "No Parent Category",
+          value: null,
+          _id: null,
+        },
+      ],
     });
   } catch (error) {
     res
