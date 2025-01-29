6996c889330b11caec29789e659d6e9778cd7e43
Fahad Hasan <44fahadhasan@gmail.com>
2025-01-29 11:55:26 +0600

    product post api done

--- models/Product.js
+++ models/Product.js
@@ -1,201 +1,132 @@
 // External imports
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// Define the meta schema
+const metaSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+  },
+  property: {
+    type: String,
+    trim: true,
+  },
+  content: {
+    type: String,
+    trim: true,
+  },
+});
+
+// Define the subsection schema
+const SubsectionSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    min: 0,
+    id: { type: Number, required: [true, "Product id number is required"] },
+  },
+  attribute_name: { type: String, trim: true },
+  attribute_values: { type: [String] },
+});
+
+// Define the variant schema
+const VariantSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    min: 0,
+    required: [true, "Product id number is required"],
+  },
+  image_url: { type: String, trim: true },
+  sku: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, "Product sku is required"],
+  },
+  product_quantity: {
+    type: Number,
+    min: 0,
+    required: [true, "Product quantity is required"],
+  },
+  product_price: {
+    type: Number,
+    min: 0,
+    required: [true, "Product price is required"],
+  },
+  stock_status: {
+    type: String,
+    enum: {
+      values: ["in stock", "out of stock", "pre order"],
+      message:
+        'Stock must be either "in stock", "out of stock", or "pre order".',
+    },
+    default: "in stock",
+  },
+  subsections: { type: [SubsectionSchema] },
+});
 
 // Define the product schema
 const productSchema = new mongoose.Schema(
   {
-    address: {
-      houseNumber: {
-        type: String,
-        trim: true,
-        // required: [true, "House number is required"],
-      },
-      road: {
-        type: String,
-        trim: true,
-        required: [true, "Road name is required"],
-      },
-      area: {
-        type: String,
-        trim: true,
-        required: [true, "Area name is required"],
-      },
-      country: {
-        type: String,
-        trim: true,
-        required: [true, "Country is required"],
-        // lowercase: true,
-      },
-      division: {
-        type: String,
-        trim: true,
-        // lowercase: true,
-        required: [true, "Division is required"],
-      },
-      districtsOrThana: {
-        type: String,
-        trim: true,
-        // lowercase: true,
-        required: [true, "Districts or Thana is required"],
-      },
-    },
-    apartmentDetails: {
-      description: {
-        type: String,
-        required: [true, "Description field is required"],
-      },
-      images: {
-        type: [String],
-        required: [true, "Images field is required"],
-        // validate: {
-        //   validator: function (images) {
-        //     return images.every((image) =>
-        //       /^https?:\/\/.+\.(jpg|jpeg|png|gif|webp)$/.test(image)
-        //     );
-        //   },
-        //   message:
-        //     "All images must be valid URLs with supported formats (jpg, jpeg, png, gif, webp)",
-        // },
-      },
-      video: {
-        type: String,
-        // validate: {
-      },
-      size: {
-        type: Number,
-        required: [true, "Size in square feet is required"],
-        min: [1, "Size must be at least 1 square foot"],
-      },
-      bedrooms: {
-        type: Number,
-        required: [true, "Number of bedrooms is required"],
-        min: [1, "At least one bedroom is required"],
-      },
-      balconies: {
-        type: Number,
-        required: [true, "Number of balconies is required"],
-        min: [0, "Balconies cannot be negative"],
-      },
-      toilets: {
-        type: Number,
-        required: [true, "Number of toilets is required"],
-        min: [1, "At least one toilet is required"],
-      },
-      drawingDining: {
-        type: Number,
-        required: [true, "Number of Drawing/dining room is required"],
-        min: [1, "At least one Drawing/dining is required"],
-      },
-      kitchen: {
-        type: Number,
-        required: [true, "Number of kitchen is required"],
-        min: [1, "At least one kitchen is required"],
-      },
-    },
-    buildingDetails: {
-      floorPosition: {
-        type: [String],
-        validate: {
-          validator: function (v) {
-            return Array.isArray(v) && v.length > 0;
-          },
-          message: "At least one floor position is required",
-        },
-      },
-      unitsPerFloor: {
-        type: Number,
-        required: [true, "Units per floor are required"],
-        min: [1, "There must be at least one unit per floor"],
-      },
-      lpgGas: {
-        type: String,
-        // lowercase: true,
-        enum: {
-          values: ["Yes", "No"],
-          message: "lpgGas must be either 'Yes' or 'No'",
-        },
-        required: [true, "lpgGas is required"],
-      },
-      parkingArea: {
-        type: String,
-        // lowercase: true,
-        enum: {
-          values: ["Yes", "No"],
-          message: "Parking area must be either 'Yes' or 'No'",
-        },
-        required: [true, "Parking area is required"],
-      },
-    },
-    price: {
-      ratePerSft: {
-        type: Number,
-        required: [true, "Rate per square foot is required"],
-      },
-    },
-    orientation: {
+    product_name: {
       type: String,
-      required: [true, "Orientation is required"],
+      trim: true,
+      required: [true, "Product name is required"],
     },
-    completionStatus: {
-      percentage: {
-        type: Number,
-        min: [0, "Completion percentage cannot be less than 0"],
-        max: [100, "Completion percentage cannot exceed 100"],
-      },
-      condition: {
-        type: String,
-        // lowercase: true,
-        enum: {
-          values: ["New", "Used"],
-          message: "Condition must be either 'New' or 'Used'",
-        },
-        required: [true, "Condition is required"],
-      },
-    },
-    properties: {
-      type: {
-        type: String,
-        // lowercase: true,
-        enum: {
-          values: ["Residential", "Commercial"],
-          message: "Property type must be 'Residential' or 'Commercial'",
-        },
-        required: [true, "Property type is required"],
-      },
-    },
-    objective: {
-      status: {
-        type: String,
-        // lowercase: true,
-        enum: {
-          values: ["Buy", "Rent", "Sell"],
-          message: "Objective status must be either 'Buy' or 'Rent' or 'Sell'",
-        },
-        required: [true, "Objective status is required"],
-      },
-    },
-    featuredProperty: {
+    product_category: {
       type: String,
-      // lowercase: true,
+      trim: true,
+      required: [true, "Product category is required"],
+    },
+    product_tag: { type: String, trim: true },
+    product_brand: { type: String, trim: true },
+    featured_product: {
+      type: String,
       enum: {
-        values: ["Yes", "No"],
-        message: "Featured property must be either 'Yes' or 'No'",
+        values: ["yes", "no"],
+        message: 'Featured product must be either "yes" or "no".',
       },
-      required: [true, "Featured property is required"],
+      default: "no",
+    },
+    product_images_url: [{ image: { type: String, trim: true } }],
+    product_video_link: { type: String, trim: true },
+    discount_type: {
+      type: String,
+      enum: ["percentage", "direct"],
+      default: "percentage",
+    },
+    discount_percentage: { type: Number, min: 0, max: 100 },
+    variants: { type: [VariantSchema] },
+    meta_info: {
+      type: [metaSchema],
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["published", "archived", "draft"],
+        message: 'Status must be either "published", "archived", or "draft".',
+      },
+      default: "published",
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
       lowercase: true,
+      trim: true,
+      required: [true, "Email is required"],
       match: [
         /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        "Please enter a valid email address",
+        "Please enter a valid email address.",
       ],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      immutable: true, // Ensures createdAt cannot be changed
     },
   },
   {
-    timestamps: true,
-    versionKey: false,
+    timestamps: true, // Adds createdAt and updatedAt fields
+    versionKey: false, // Removes the __v field
   }
 );
 
